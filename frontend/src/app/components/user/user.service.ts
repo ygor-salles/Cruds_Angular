79140dc19abc16bf2c9bd5a83161c426b753facb
unknown <ygorsallesaniceto@gmail.com>
2020-09-19 18:54:30 -0300

    Finalizando crud-user

--- frontend/src/app/components/user/user.service.ts
+++ frontend/src/app/components/user/user.service.ts
@@ -1,3 +1,6 @@
+import { EMPTY } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { User } from './user.model';
 import { Observable } from 'rxjs';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -10,37 +13,58 @@ import { Injectable } from '@angular/core';
 export class UserService {
 
   baseUrl = 'http://localhost:3001/users';
-  
+
   constructor(private http: HttpClient, private snackBar: MatSnackBar) { }
 
-  showMessage(msg: string): void {
+  showMessage(msg: string, isError: boolean = false): void {
     this.snackBar.open(msg, 'X', {
       duration: 3000,
       horizontalPosition: 'right',
-      verticalPosition: 'top'
+      verticalPosition: 'top',
+      panelClass: isError ? ['msg-error'] : ['msg-success']
     })
   }
 
   create(user: User): Observable<User> {
-    return this.http.post<User>(this.baseUrl, user)
+    return this.http.post<User>(this.baseUrl, user).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    )
+  }
+
+  errorHandler(e: any): Observable<any> {
+    this.showMessage('Ococrreu um erro!', true)
+    return EMPTY
   }
 
   read(): Observable<User[]> {
-    return this.http.get<User[]>(this.baseUrl)
+    return this.http.get<User[]>(this.baseUrl).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    )
   }
 
   readById(id: number): Observable<User> {
     const url = `${this.baseUrl}/${id}`
-    return this.http.get<User>(url)
+    return this.http.get<User>(url).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    )
   }
-  
+
   update(user: User): Observable<User> {
     const url = `${this.baseUrl}/${user.id}`
-    return this.http.put<User>(url, user)
+    return this.http.put<User>(url, user).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    )
   }
 
   delete(id: number): Observable<User> {
     const url = `${this.baseUrl}/${id}`
-    return this.http.delete<User>(url)
+    return this.http.delete<User>(url).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    )
   }
 }
--- frontend/src/app/views/user-crud/user-crud.component.ts
+++ frontend/src/app/views/user-crud/user-crud.component.ts
@@ -1,3 +1,4 @@
+import { HeaderService } from './../../components/template/header/header.service';
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
@@ -8,7 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserCrudComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private headerService: HeaderService) {
+    headerService.headerData = {
+      title: 'Cadastro de Usuários',
+      icon: 'add',
+      routeUrl: '/users'
+    }
+  }
 
   ngOnInit(): void {
   }
